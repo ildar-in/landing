317f6b7ed5e7e6d2ddcf3b5d2248c04cdd174447
ildarin <mail.ildarin@gmail.com>
2024-12-04 06:42:44 +0500

    sculp

--- game/battle-fist-sculptor/script.js
+++ game/battle-fist-sculptor/script.js
@@ -1,6 +1,9 @@
 ﻿const w = 50, h=25, size=20
 const width = w*size, height=h*size
 
+const fps = 60
+const dt = 1000/fps
+
 const blocks = []
 for(let i=0;i<w;i++){
     blocks.push([])
@@ -21,12 +24,7 @@ for(let i=0;i<w;i++){
     }
 }
 
-for(let i=25;i<40;i++){
-    for(let j=15;j<25;j++){
-        blocks[i][j].value=100
-        updateBlockView(i,j)
-    }
-}
+resetSculpture()
 
 var pressedKeys = {};
 window.onkeyup = function(e) { pressedKeys[e.code] = false; }
@@ -38,13 +36,28 @@ const char = {
     dx:0,dy:0,
     jumpAmount:2,
     speed:7,
-    freezeDyElapsed:0
+    freezeDyElapsed:0,
+    isFlip:false
 }
 
 const effects=[]
 const attackSize = 40
 
 const charHtml = createDiv(document.body,char.x,char.y,char.w,char.h,tag='div')
+
+setInterval(()=>{
+    if(char.isFlip){
+        charHtml.style.transform='scaleX(-1)'
+    }else{
+        charHtml.style.transform='scaleX(1)'
+    }
+}, dt)
+
+document.body.addEventListener('keyup',e=>{
+    if(e.code === 'KeyR'){
+        resetSculpture()
+    }
+})
 
 document.body.addEventListener('keydown',e=>{
     if(e.code === 'KeyW' && char.jumpAmount>0){
@@ -54,39 +67,52 @@ document.body.addEventListener('keydown',e=>{
 })
 
 document.body.addEventListener('mousedown',e=>{
+
+    let x = char.x
+    let y = char.y
     char.freezeDyElapsed=150
-    createEffect(char.x,char.y)
-    playAudio('content/audio/17_orc_atk_sword_1.wav')
+    
+    let diff = [e.pageX - char.x, e.pageY - char.y]
+    diff = normalizeVector(diff)
+    diff = multVector(diff, 25)
+    x = char.x+diff[0]
+    y = char.y+diff[1]
+    //x = e.pageX;
+    //y = e.pageY;
+
+    createEffect(x,y,attackSize,attackSize)
+    if(Math.random()<=0.33){playAudio('content/audio/17_orc_atk_sword_1.wav', 0.3)}
+    else if(Math.random()<=0.66){playAudio('content/audio/17_orc_atk_sword_2.wav', 0.3)}
+    else {playAudio('content/audio/17_orc_atk_sword_3.wav', 0.3)}
 
     let damagedBlocks=[]
     getAllBlocks(blocks).forEach(b=>{
         if(b.value<=0){return}
-        const distance = Math.sqrt((b.x-char.x)*(b.x-char.x) + (b.y-char.y)*(b.y-char.y))
+        const distance = Math.sqrt((b.x-x)*(b.x-x) + (b.y-y)*(b.y-y))
         if(distance<attackSize){
             b.value -= 25
             if(b.value<=0){b.value=0}
             updateBlockView(b.i,b.j)
-            //isDamage=true
             damagedBlocks.push(b)
-            //
         }
     })
     if(damagedBlocks.length>0){
-        playAudio('content/audio/26_sword_hit_1.wav')
+        playAudio('content/audio/26_sword_hit_1.wav', 0.3)
     }
 })
 
-const fps = 60
-const dt = 1000/fps
+//----------------
 
 let gravity = 1
 setInterval(()=>{
 
     if(pressedKeys['KeyA']){
+        char.isFlip=true
         if(!getCharacterBlocked(-char.speed, 0)){
             char.x-=char.speed
         }
     }else if(pressedKeys['KeyD']){
+        char.isFlip=false
         if(!getCharacterBlocked(char.speed, 0)){
             char.x+=char.speed
         }
@@ -116,11 +142,18 @@ setInterval(()=>{
     //charHtml.style.backgroundColor='#0000cc'
     charHtml.style.backgroundImage ='url("content/image/3.png")'
     charHtml.style.backgroundSize='contain'
-    
-
 }, dt)
 
 //---------------
+
+function resetSculpture(){
+    for(let i=25;i<40;i++){
+        for(let j=15;j<25;j++){
+            blocks[i][j].value=100
+            updateBlockView(i,j)
+        }
+    }
+}
 
 function getCharacterBlocked(dx,dy){
     const block = getAllBlocks().find(b=>{
@@ -132,34 +165,6 @@ function getCharacterBlocked(dx,dy){
         return false
     })
     return block
-    //return id!==-1
-}
-
-
-// Javascript program to Check if a 
-// point lies on or inside a rectangle | Set-2 
- 
-// function to find if given point 
-// lies inside a given rectangle or not. 
-function FindPoint(x1, y1, x2, y2, x, y) 
-{ 
-    return x > x1 && x < x2 && y > y1 && y < y2
-} 
-
-function isRectanglesCollides(x1,y1,w1,h1, x2,y2,w2,h2) {
-	return overlaps({x1:x1,y1:y1, x2:x1+w1,y2:y1+h1},{x1:x2,y1:y2, x2:x2+w2,y2:y2+h2});
-}
-// Check if rectangle a overlaps rectangle b
-// Each object (a and b) should have 2 properties to represent the
-// top-left corner (x1, y1) and 2 for the bottom-right corner (x2, y2).
-function overlaps(a, b) {
-	// no horizontal overlap
-	if (a.x1 >= b.x2 || b.x1 >= a.x2) return false;
-
-	// no vertical overlap
-	if (a.y1 >= b.y2 || b.y1 >= a.y2) return false;
-
-	return true;
 }
 
 function updateBlockView(i,j){
@@ -193,8 +198,8 @@ function createBlockInfo(i,j,value=0){
     }
 }
 
-function createEffect(x,y){
-    const effectHtml = createDiv(document.body,x,y,attackSize,attackSize,tag='div')
+function createEffect(x,y,w,h){
+    const effectHtml = createDiv(document.body,x-w/2,y-h/2,w,h,tag='div')
     //effectHtml.style.backgroundColor ='#ffcc33'
     //effectHtml.style.border ='1px solid #ffcc33'
     effectHtml.style.backgroundImage ='url("content/image/4.png")'
@@ -204,8 +209,11 @@ function createEffect(x,y){
     }, 500)
 }
 
-function playAudio(path){
-    new Audio(path).play()
+function playAudio(path,volume=1){
+    var a = new Audio(path)
+    a.volume = volume
+    a.play()
+    return a
 }
 
 function createDiv(parent,x,y,w,h,tag='div'){
@@ -217,4 +225,44 @@ function createDiv(parent,x,y,w,h,tag='div'){
     cellHtml.style.width = w
     cellHtml.style.height = h
     return cellHtml
+}
+
+
+//-----------------
+
+function normalizeVector(v=[]){
+    var x=v[0],y=v[1];
+    var length = Math.sqrt(x**2+y**2);
+    //Then divide the x and y by the length.
+    x = x/length;
+    y = y/length;
+    return [x,y]
+}
+function multVector(v=[], m=1){
+    return [v[0]*m,v[1]*m]
+}
+
+// Javascript program to Check if a 
+// point lies on or inside a rectangle | Set-2 
+ 
+// function to find if given point 
+// lies inside a given rectangle or not. 
+function FindPoint(x1, y1, x2, y2, x, y) 
+{ 
+    return x > x1 && x < x2 && y > y1 && y < y2
+} 
+function isRectanglesCollides(x1,y1,w1,h1, x2,y2,w2,h2) {
+	return overlaps({x1:x1,y1:y1, x2:x1+w1,y2:y1+h1},{x1:x2,y1:y2, x2:x2+w2,y2:y2+h2});
+}
+// Check if rectangle a overlaps rectangle b
+// Each object (a and b) should have 2 properties to represent the
+// top-left corner (x1, y1) and 2 for the bottom-right corner (x2, y2).
+function overlaps(a, b) {
+	// no horizontal overlap
+	if (a.x1 >= b.x2 || b.x1 >= a.x2) return false;
+
+	// no vertical overlap
+	if (a.y1 >= b.y2 || b.y1 >= a.y2) return false;
+
+	return true;
 }
